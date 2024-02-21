a4e8fb5204e58abe8948f9dc62f6d294f859b040
Даша Старостюк <starostiukdasha@gmail.com>
2024-02-21 16:33:22 +0200

    3 працюючі кнопки для оболонки

--- Button.hpp
+++ Button.hpp
@@ -1,24 +1,29 @@
-#ifndef BUTTON_HPP
-#define BUTTON_HPP
+#ifndef BUTTON_H
+#define BUTTON_H
 
 #include <SFML/Graphics.hpp>
-#include <string>
+#include <iostream>
+#include <functional>
 
 class Button {
 public:
     Button(sf::Texture* normal, sf::Texture* clicked, std::string text, sf::Vector2f location);
     void checkClick(sf::Vector2f mousePos);
-    void setState(bool state);
-    void setText(std::string text);
+    void setState(bool);
     bool getState();
     sf::Sprite* getSprite();
     sf::Text* getText();
+    void setAction(std::function<void()> action);
+    void toggleVisibility();
+    bool isVisible();
 private:
     sf::Sprite normalSprite;
     sf::Sprite clickedSprite;
     sf::Sprite* currentSprite;
     sf::Text buttonText;
     bool currentState;
+    bool visible;
+    std::function<void()> buttonAction;
 };
 
-#endif
+#endif // BUTTON_H
